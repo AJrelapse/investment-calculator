33e844f9508ad9f4c27233577982bcf05f50c59f
AJrelapse <ajayanandsm@gmail.com>
2026-05-25 16:03:41 +0530

    fix: Indian Locale

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { registerLocaleData } from '@angular/common';
+import localeIn from '@angular/common/locales/en-IN';
+
+
+registerLocaleData(localeIn);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-IN' } // ✅ Indian format
+  ]
+});
