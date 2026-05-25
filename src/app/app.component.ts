116897bb8e1d306a528aa25422e2d4f58e51e805
AJrelapse <ajayanandsm@gmail.com>
2026-05-25 09:24:03 +0530

    feat: User Input Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
